07939a7eee6d4a90935660327912709aec4af10f
devin <devinm3333@outlook.com>
2025-09-09 10:05:12 -0700

    create cat detail page

--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,7 @@
 from django.shortcuts import render
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+from .models import Cat
 
 # Create your views here.
-
-# Define the home view function
 def home(request):
     return render(request, 'home.html')
 
@@ -24,5 +9,10 @@ def about(request):
     return render(request, 'about.html')
 
 def cat_index(request):
+    cats = Cat.objects.all()
     # return render(<req>, <view>, <Object>)
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
